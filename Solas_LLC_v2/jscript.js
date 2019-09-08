b5f49a456656e431626bcc5982a0c23534f6473f
Patrick Marre <pmarre40@gmail.com>
2019-09-08 11:29:32 -0700

    Styling

--- Solas_LLC_v2/jscript.js
+++ Solas_LLC_v2/jscript.js
@@ -1,42 +1,66 @@
-const card = document.querySelectorAll('.card');
-const aboutImage = document.querySelector('.ab-img');
+let orange = 'rgba(242, 113, 39, .9)';
+let red = 'rgba(166, 36, 36, .9)';
+let green = 'rgba(20, 140, 96, .9)';
+let gold = 'rgba(242, 213, 187, .9)';
+let dark_orange = 'rgba(191, 99, 65, .9)';
+let yellow = 'rgba(242, 190, 34, .9)';
+let light_green = 'rgba(140, 191, 126, .9)';
 
-var isInViewport = function(elem) {
-  var bounding = elem.getBoundingClientRect();
-  return (
-    bounding.top >= 0 &&
-    bounding.left >= 0 &&
-    bounding.bottom <=
-      (window.innerHeight || document.documentElement.clientHeight) &&
-    bounding.right <=
-      (window.innerWidth || document.documentElement.clientWidth)
+$(document).ready(function() {
+  onMouseEnter($('.grid-item'));
+});
+
+function onMouseEnter(e) {
+  e.hover(
+    function() {
+      let id = this.id;
+
+      if (id == 'orange') {
+        $(this).css('background-color', orange);
+        mouseIn($(this));
+      }
+      if (id == 'red') {
+        $(this).css('background-color', red);
+        mouseIn($(this));
+      }
+      if (id == 'green') {
+        $(this).css('background-color', green);
+        mouseIn($(this));
+      }
+      if (id == 'gold') {
+        $(this).css('background-color', gold);
+        mouseIn($(this));
+      }
+      if (id == 'dark_orange') {
+        $(this).css('background-color', dark_orange);
+        mouseIn($(this));
+      }
+      if (id == 'yellow') {
+        $(this).css('background-color', yellow);
+        mouseIn($(this));
+      }
+      if (id == 'light_green') {
+        $(this).css('background-color', light_green);
+        mouseIn($(this));
+      }
+    },
+    function() {
+      resetHoverBackground($(this));
+      mouseOut($(this));
+    }
   );
-};
+}
 
-window.addEventListener(
-  'scroll',
-  () => {
-    var interval = 200;
-    [...card].forEach((x, index) => {
-      if (isInViewport(x)) {
-        setTimeout(() => x.classList.add('start'), index * interval);
-      }
-    });
-  },
-  false
-);
+function resetHoverBackground(e) {
+  e.css('background-color', 'rgba(0, 0, 0, 0)');
+}
 
-window.addEventListener(
-  'scroll',
-  () => {
-    var childrenEl = document.querySelector('.heading-contain').children;
+function mouseIn(e) {
+  e.find('h3').css('display', 'none');
+  e.find('.hover-detail').css('display', 'block');
+}
 
-    var interval = 100;
-    [...childrenEl].forEach((y, index) => {
-      if (isInViewport(y)) {
-        setTimeout(() => y.classList.add('animate'), index * interval);
-      }
-    });
-  },
-  false
-);
+function mouseOut(e) {
+  e.find('h3').css('display', 'block');
+  e.find('.hover-detail').css('display', 'none');
+}
